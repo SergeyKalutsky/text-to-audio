81cc484d0021a2c7fbb71fb4fce6204f86fa7f75
Sergey Kalutsky <skalutsky@gmail.com>
2022-06-20 17:10:24 +0300

    made a simple parser version(almost)

--- parser.py
+++ parser.py
@@ -1,6 +1,36 @@
+import json
 from itertools import chain
+from random import sample
 
-text = 'BTS (tiếng Hàn: 방탄소년단; Romaja: Bangtan Sonyeondan), còn được gọi là Bangtan Boys, là một nhóm nhạc nam Hàn Quốc được thành lập vào năm 2010 và ra mắt vào năm 2013 bởi Big Hit Entertainment.[5] Nhóm bao gồm 7 thành viên: Jin, Suga, J-Hope, RM, Jimin, V và Jungkook. Các thành viên đồng sáng tác và sản xuất phần lớn các sản phẩm âm nhạc của họ. Khởi đầu là một nhóm nhạc hip hop, phong cách âm nhạc của nhóm đã dần phát triển để thể hiện nhiều thể loại đa dạng. Ca từ của nhóm thường tập trung vào lời bình luận cá nhân và xã hội, đề cập đến các chủ đề về sức khỏe tinh thần, những rắc rối của tuổi học đường và tuổi thành niên, sự mất mát, hành trình hướng tới tình yêu bản thân và chủ nghĩa cá nhân. Các sản phẩm của nhóm còn thường đề cập đến khái niệm văn học, tâm lý học và bao gồm một cốt truyện vũ trụ song song.'
-text = [val.split(',') for val in text.split('.')]
-texts = [i.strip() for i in chain(*text) if i != '']
-print(texts)
+with open('Viet22K.txt', 'r', encoding='utf-8') as f:
+    viet_words = f.readlines()
+
+viet_words = [word.strip() for word in viet_words]
+
+path = 'stories/p1.txt'
+with open(path, 'r', encoding='utf-8') as f:
+    text = f.read()
+
+paragraps = [t.replace('"', '') for t in text.split('\n') if t != '']
+
+d = {}
+for i, paragraph in enumerate(paragraps):
+    text = [val.split(',') for val in paragraph.split('.')]
+    texts = [i.strip() for i in chain(*text) if i != '']
+    add = []
+
+    for word in viet_words:
+        if len(word) >= 3:
+            for sentence in texts:
+                if word in sentence:
+                    add.append(word)
+
+    sep_words = list(set(add))
+    sep_words = sample(list(set(add)), int(0.2*len(sep_words)))
+    d[i] = {}
+    d[i]['paragraph'] = paragraph
+    d[i]['sentences'] = texts
+    d[i]['words'] = sep_words
+
+with open('parsed.json', 'w', encoding='utf-8') as f:
+     json.dump(d, f)
